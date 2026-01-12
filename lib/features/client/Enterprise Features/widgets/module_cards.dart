9a0b1373bbf0186223905e3f27a6a82939682a7a
Faysal-Maxamed <pheymoha17@gmail.com>
2026-01-12 16:48:39 +0300

    ..

--- lib/features/client/Enterprise Features/widgets/module_cards.dart
+++ lib/features/client/Enterprise Features/widgets/module_cards.dart
@@ -12,26 +12,24 @@ class ModuleCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 120,
-      decoration: BoxDecoration(
-        color: Color(0xffDFD7D0).withOpacity(0.3),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 55,
-            width: 55,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-            child: Image.asset(moduleImage, width: 35, height: 35),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 65,
+          width: 65,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Color(0xffDFD7D0).withOpacity(0.3),
           ),
-          SizedBox(height: 5),
-          Text(moduleName, style: GoogleFonts.poppins(fontSize: 12)),
-        ],
-      ),
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Image.asset(moduleImage, fit: BoxFit.cover),
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(moduleName, style: GoogleFonts.poppins(fontSize: 12)),
+      ],
     );
   }
 }
