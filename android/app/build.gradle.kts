aebae3ccb26823cd9d39c4a6aa8c2a3e0656a8f4
Pheyzal <pheyzalmoha@gmail.com>
2026-08-24 12:36:23 +0300

    ..

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
     
     signingConfigs {
